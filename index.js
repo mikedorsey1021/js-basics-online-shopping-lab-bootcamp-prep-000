2fe7129d20ed0555d99ffcc52a78e933620f9484
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-09 11:45:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,9 +29,10 @@ function viewCart() {
     return responsePart1 + cart[0].itemName + ' at $' + cart[0].itemPrice +'.';
   }else {
     var i;
-    var test = cart[i].itemName;
-    for (i = 0; i < cart.length; i++){
+    var test; 
 
+    for (i = 0; i < cart.length; i++){
+      var test = cart[i].itemName;
       // var item = cart[i].itemName;
       // var price = cart[i].itemPrice;
     }
