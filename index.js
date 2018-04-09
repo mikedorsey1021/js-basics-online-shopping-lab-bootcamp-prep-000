05941179286b80f51b8f5dc2e0c377c1f78b7c30
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-09 15:12:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function viewCart() {
   var responsePart1 = 'In your cart, you have ';
   var spacer = ' at $';
 
-
+  var cart = getCart(); 
   if (cart.length === 0){
     return 'Your shopping cart is empty.'
   }else if (cart.length === 1) {
